c0952fb776383c9d834ce2898cdf28c03eac0bd0
andreagulp <andreacommunication@gmail.com>
2019-09-11 16:51:28 +0200

    employee can be added to mongo

--- client/src/actions/employees_action.js
+++ client/src/actions/employees_action.js
@@ -3,13 +3,6 @@ import axios from "axios";
 import { ADD_EMPLOYEE } from "./types";
 
 export const addEmployee = employee => async dispatch => {
-  const newEmployee = {
-    name: "Andrea",
-    lastName: "Riccetti",
-    iconsUrl:
-      "https://previews.123rf.com/images/solargaria/solargaria1709/solargaria170900008/85425363-user-icon-male-with-beard-icon-hipster-flat-icon-avatar-of-man-with-beard-flat-internet-icon-in-roun.jpg",
-    jobRole: "Manager"
-  };
-  const res = await axios.post("/api/new_employee", newEmployee);
+  const res = await axios.post("/api/new_employee", employee);
   dispatch({ type: ADD_EMPLOYEE, payload: res });
 };
